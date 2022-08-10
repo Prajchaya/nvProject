47124b8f215d08d1c5892fdfca78c6fe4bd4b051
Prajchaya Arunrat <prajehayarock@gmail.com>
2022-08-10 12:42:50 +0700

    8/10/22

--- App.js
+++ App.js
@@ -1,56 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, Button, TextInput } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React from "react";
+import FirstPage from "./screen/FirstPage";
+import SecondPage from "./screen/SecondPage";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-
-
-function HomeScreen({ navigation, route }) {
-
-  React.useEffect(() => {
-    if (route.params?.post) {
-      //Post updated,do somethin with 'route.params.post'
-      //For example, sent the post to the server      
-    }
-  }, [route.params?.post]);
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        title='Create Post'
-        onPress={() => navigation.navigate('CreatePost')}
-      />
-      <Text style={{ margin: 10 }}>Post:{route.params?.post}</Text>
-    </View>
-  );
-}
-
-function CreatePostScreen({ navigation, route }) {
-
-  const [postText, setPostText] = React.useState('');
-
-  return (
-    // use Fragment
-    <>
-      <TextInput
-        multiline
-        placeholder='Plese text here'
-        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
-        onChangeText={setPostText}
-        value={postText}
-      />
-      <Button
-        title='Click'
-        onPress={() => {
-          //Pass params back to HomeScreen funchion
-          navigation.navigate('Home', { post: postText })
-        }}
-      />
-    </>
-
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
@@ -58,17 +11,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName='Home'
+        initialRouteName="FirstPage"
         screenOptions={{
-          headerStyle: { backgroundColor: 'blue' },
-          headerTintColor: '#ffff',
-          headerTitleStyle: { fontWeight: 'bold' }
+          headerStyle: { backgroundColor: "blue" },
+          headerTintColor: "#ffff",
+          headerTitleStyle: { fontWeight: "bold", fontSize: 20 },
         }}
       >
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='CreatePost' component={CreatePostScreen} />
+        <Stack.Screen name="FirstPage" component={FirstPage} />
+        <Stack.Screen name="SecondPage" component={SecondPage} />
       </Stack.Navigator>
     </NavigationContainer>
-
   );
 }
---
+++ screen/SecondPage.js
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SecondPage = ({ navigation, route }) => {
+    const [postText, setPostText] = React.useState();
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.heading}>Thai-Nichi Institute of Technology</Text>
+          <Text style={styles.textStyle}>
+            Values passed from First page : {route.params?.post}
+          </Text>
+          
+        </View>
+      </View>
+      <View style={{ alignItems: "center" }}>
+        <Text
+          style={{
+            color: "gray",
+          }}
+        >
+          www.tni.ac.th
+        </Text>
+      </View>
+    </>
+  )
+}
+
+export default SecondPage
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        padding: 20,
+    },
+    heading: {
+        fontSize: 25,
+        textAlign: 'center',
+        marginVertical: 10,
+    },
+    textStyle: {
+        textAlign: 'center',
+        fontSize: 16,
+        marginVertical: 10,
+    },
+})

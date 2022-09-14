04635db9b6841ff0f7ec88b4f220e9f7d350b7b1
Prajchaya Arunrat <prajehayarock@gmail.com>
2022-09-14 10:19:18 +0700

    9/14/2022

--- App.js
+++ App.js
@@ -1,22 +1,26 @@
 import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { Ionicons } from '@expo/vector-icons'
+import HomeScreen from "./screen/HomeScreen";
+
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
-function HomeScreen({navigation}){
-  return(
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home!</Text>
-      <Button 
-        title="GO TO SETTING" 
-        onPress={()=>
-          navigation.navigate("Setting")
-        }
-      />
-    </View>
-  )
-}
+// function HomeScreen({navigation}){
+//   return(
+//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+//       <Text>Home!</Text>
+//       <Button 
+//         title="GO TO SETTING" 
+//         onPress={()=>
+//           navigation.navigate("Setting")
+//         }
+//       />
+//     </View>
+//   )
+// }
 
 function Setting({navigation}){
   return(
@@ -33,20 +37,50 @@ function Setting({navigation}){
 }
 
 const Tab = createBottomTabNavigator();
-
 function MyTabs(){
   return(
-   <Tab.Navigator>
-     <Tab.Screen name='Home' component={HomeScreen}/>
-     <Tab.Screen name='Setting' component={Setting}/>
-   </Tab.Navigator>
+    <Tab.Navigator
+     screenOptions={({route})=>({
+      tabBarIcon: ({focused,color,size})=>{
+        let iconName;
+        if(route.name==='Home'){
+          iconName = focused
+          ?'ios-information-circle'
+          :'ios-information-circle-outline'
+        } else if(route.name==='Setting'){
+          iconName = focused
+          ?'ios-list-box'
+          :'ios-list'
+        }
+        return<Ionicons name={iconName} size={size} color={color}/>
+      },
+      tabBarActiveTintColor: 'tomato',
+      tabBarInactiveTintColor : 'gray',
+
+     }
+     )
+    }
+    >
+      <Tab.Screen name="Home" component={HomeScreen}/>
+      <Tab.Screen name="Setting" component={Setting}/>
+
+    </Tab.Navigator>
   )
 }
 
+const Drawer = createDrawerNavigator();
+function MyDrawer(){
+  return(
+    <Drawer.Navigator useLegacyImplementation>
+      <Drawer.Screen name='Home' component={MyTabs}/>
+      <Drawer.Screen name='Setting' component={Setting}/>
+    </Drawer.Navigator>
+  );
+}
 const App = () => {
   return (
     <NavigationContainer>
-      <MyTabs/>
+      <MyDrawer/>
     </NavigationContainer>
   )
 }

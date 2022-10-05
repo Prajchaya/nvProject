eae99c5f918c6b9b9f0629b523d34f3b9cf8d6ff
Prajchaya Arunrat <prajehayarock@gmail.com>
2022-10-05 11:10:09 +0700

    10/5/22

--- App.js
+++ App.js
@@ -65,84 +65,104 @@
 
 // const styles = StyleSheet.create({});
 
-import { StyleSheet, Text, View, Button, Image, SafeAreaView} from "react-native";
+import { StyleSheet, Text, View, Button,Image ,SafeAreaView} from "react-native";
 import React from "react";
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from "./screen/ProductScreen";
 import DetailScreen from "./screen/DetailScreen";
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
-import { NavigationContainer,DefaultTheme } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { 
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import {
   createDrawerNavigator,
   DrawerContentScrollView,
   DrawerItemList,
-  DrawerItem
+  DrawerItem,
 } from "@react-navigation/drawer";
- 
+
+
 const MyTheme = {
-  ...DefaultTheme, 
-  colors:{
+  ...DefaultTheme,
+  colors: {
     ...DefaultTheme.colors,
-    primary:'rgb(255,45,85)'
-  }
+    primary: "rgb(255,45,85)",
+  },
+};
 
-}
 
-function CustomDrawerContent(props){
-  return(
-    <SafeAreaView style = {{flex : 1}}>
-      <Image style={styles.sideMenuProfileIcon}
-      source={require("./assets/react_logo.png")}
+
+function CustomDrawerContent(props) {
+  return (
+    <SafeAreaView>
+      <Image 
+        style={styles.sideMenuProfileIcon}
+        source={require("./assets/react_logo.png")}
       />
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props}/>
-      <DrawerItem 
-        label='Close drawer' 
-        onPress={()=> props.navigation.closeDrawer()} 
-      />
-      
-    </DrawerContentScrollView>
+      <DrawerContentScrollView {...props}>
+        <DrawerItemList {...props} />
+        <DrawerItem
+          label="Close drawer"
+          onPress={() => props.navigation.closeDrawer()}
+        />
+      </DrawerContentScrollView>
     </SafeAreaView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function ProductStack(){
+  return(
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle:{
+          backgroundColor:'#0096DA'
+        },
+        headerTintColor:'#ffff',
+        headerTitleStyle:{
+          fontWeight:'bold'
+        }
+      }}
+    >
+        <Stack.Screen name="Product" component={ProductScreen}/>
+        <Stack.Screen name="Detail" component={DetailScreen}/>
+    </Stack.Navigator>
   )
 }
+
 const Drawer = createDrawerNavigator();
-
-
-function MyDrawer(){
-  return(
-    <Drawer.Navigator 
-    useLegacyImplementation
-    drawerContent={(props)=><CustomDrawerContent {...props}/>}
-    screenOptions={{
-      drawerStyle:{
-        backgroundColor: 'white',
-        width:240
-      }
-    }}
+function MyDrawer() {
+  return (
+    <Drawer.Navigator
+      useLegacyImplementation
+      drawerContent={(props) => <CustomDrawerContent {...props} />}
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "white",
+          width: 240,
+        },
+      }}
     >
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Product" component={ProductScreen} />
-        <Drawer.Screen name="Detail" component={DetailScreen} />
-      </Drawer.Navigator>
-  )
+      <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Product" component={ProductStack} />
+    </Drawer.Navigator>
+  );
 }
 const App = () => {
   return (
     <NavigationContainer theme={MyTheme}>
-      <MyDrawer/>
+      <MyDrawer />
     </NavigationContainer>
   );
 };
 
-export default App;
-
 const styles = StyleSheet.create({
   sideMenuProfileIcon: {
-    resizeMode: 'center',
+    resizeMode: "center",
     width: 100,
     height: 100,
     borderRadius: 100 / 2,
-    alignSelf: 'center',
+    alignSelf: "center",
   },
-})
+});
+export default App;
--- screen/ProductScreen.js
+++ screen/ProductScreen.js
@@ -1,96 +1,188 @@
-import { StyleSheet, Text, View, ActivityIndicator, FlatList,Image } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import {View, Text, StyleSheet, FlatList, ActivityIndicator, Image} from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState, useEffect, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
+import axios from "axios";
+
+
+const _renderItems = ({ item }) => {
+const ItemSeparatorView = () => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Image
+          // resizeMode='center'
+          source={{ uri: item.picture }}
+          style={styles.thumbnail}
+        />
+      </View>
+      <View style={styles.dataContent}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.detail}>{item.detail}</Text>
+      </View>
+    </View>
+    // Flat List Item Separator
+    <View
+      style={{
+        height: 0.5,
+        width: "100%",
+        backgroundColor: "#C8C8C8",
+      }}
+    />
+  );
+};
+const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: "100%",
+          backgroundColor: "#C8C8C8",
+        }}
+      />
+    );
+  };
 
 const ProductScreen = () => {
-    const [product, setProduct] = useState([]);
-    
-    const ItemSeparatorView = () => {
-        return (
-          // Flat List Item Separator
-          <View
-            style={{
-              height: 0.5,
-              width: '100%',
-              backgroundColor: '#C8C8C8',
-            }}
-          />
-        );
-      };
-    const getData = async() =>{
-            const res = await axios.get('https://api.codingthailand.com/api/course');
-            console.log(res.data.data)
-            alert(JSON.stringify(res.data.data))
-            setProduct(res.data.data);
+const ProductScreen = ({navigation}) => {
+  const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getData = async () => {
+    const res = await axios.get("https://api.codingthailand.com/api/course");
+    setProduct(res.data.data);
+    try {
+      setLoading(true);
+      const res = await axios.get("https://api.codingthailand.com/api/course");
+      setProduct(res.data.data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error); //set errorr ไปที่ state ของ error ว่าเกิดจาก axios หรือ ตัวอื่น
     }
+  };
 
-    useEffect(()=>{
-        getData();
-    },[])
+  useFocusEffect(
+    useCallback(() => {
+      getData();
+    }, [])
+  );
+  /*useEffect(() => {
+    getData();
+  }, []);*/
 
-    const _renderItem = ({item})=> {
-        return(
-        <View style={{flex:1}}>
-            <View style={{flex:1,flexDirection:'row', margin:5}}>
+  if (error) {
+    //ถ้ามี error เกิดขึ้นจาก return ui ต่อไปนี้กลับไป
+    return (
+      <View style={styles.container}>
+        <Text>{error.message}</Text>
+        <Text>เกิดข้อผิดพลาด ไม่สามารถติดต่อกับ server ได้</Text>
+      </View>
+    );
+  }
+
+  if (loading == true) {
+    return (
+      <View>
+        <ActivityIndicator color="#f4511e" size="large" />
+      </View>
+    );
+  }
+  const _renderItems = ({ item }) => {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <TouchableOpacity
+          style={styles.addButtonStyle}
+          onPress= {() => {
+            navigation.navigate('Detail', {
+              id:item.id,
+              title:item.title
+            })
+          }}
+        >
+          <View style={styles.container}>
+            <View>
               <Image
-              resizeMode='cover'
-              source={{ uri: item.picture }}
-              style = {styles.thumbnail}
+                // resizeMode='center'
+                source={{ uri: item.picture }}
+                style={styles.thumbnail}
               />
-              
-              <View style={styles.dataContainer}>
-                <View style={styles.dataContent}>
-                    <Text style={styles.title}>{item.title}</Text>
-                    <Text style={styles.detail}>{item.detail}</Text>
-                </View>
-              </View>
+            </View>
+            <View style={styles.dataContent}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.detail}>{item.detail}</Text>
             </View>
           </View>
-        )
-    }
+        </TouchableOpacity>
+      </SafeAreaView>
+    );
+  };
+  useEffect(() => {
+  const _onRefresh = () => {
+    getData();
+  }, []);
+};
 
   return (
-    <View style={styles.dataContainer}>
+    <View>
       <FlatList
-      data = {product}
-      keyExtractor = {(item, index) => item.id.toString()}
-      ItemSeparatorComponent = {ItemSeparatorView}
-      renderItem = {_renderItem}
+        data={product}
+        keyExtractor={(item, index) => item.id.toString()}
+        ItemSeparatorComponent={ItemSeparatorView}
+        renderItem={_renderItems}
+        refreshing={loading}
+        onRefresh={_onRefresh}
       />
     </View>
-  )
-}
-
-export default ProductScreen
-
+  );
+};
+export default ProductScreen;
 const styles = StyleSheet.create({
-    container: {
-        height: 80,
-        elevation: 3,
-        borderColor: 'gray',
-        borderRadius: 5,
-        flexDirection: 'row',
-        marginHorizontal: 20,
-    },
-    dataContainer: {
-        flex: 1,
-    },
-    thumbnail: {
-        width: 70,
-        height: 70,
-    },
-    dataContent: {
-        marginTop: 5,
-        marginLeft: 15,
-    },
-    title: {
-        color: '#444',
-        fontSize: 14,
-        fontWeight: 'bold',
-    },
-    detail: {
-        fontSize: 12,
-        color: '#888',
-        fontWeight: '700',
-    },
-})
+  container: {
+    height: 80,
+    elevation: 3,
+    borderColor: "gray",
+    borderRadius: 5,
+    flexDirection: "row",
+    maeginHorizontal: 20,
+    margin: 5,
+  },
+  dataContainer: {
+    flex: 1,
+  },
+  thumbnail: {
+    width: 70,
+    height: 70,
+  },
+  dataContent: {
+    marginTop: 5,
+    marginLeft: 15,
+  },
+  title: {
+    color: "#444",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  detail: {
+    fontSize: 16,
+    color: "#777",
+    fontWeight: 700,
+  },
+});
+  addButtonStyle: {
+    width: "100%",
+    marginBottom : 15
+  }
+};
